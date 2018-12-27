500b901fc785b80e325c788dfe59ee9f83537864
bartvanhoey <bartvanhoey@hotmail.com>
2018-12-27 05:56:17 +0100

    end part 38

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { CustomPreloadingService } from './custom-preloading.service';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes, PreloadAllModules, NoPreloading } from '@angular/router';
 import { HomeComponent } from './home.component';
@@ -7,14 +8,15 @@ const appRoutes: Routes = [
   { path: 'home', component: HomeComponent },
   // redirect to the home route if the client side route path is empty
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'employees', loadChildren: './employee/employee.module#EmployeeModule'},
+  { path: 'employees', data: { preload: true }, loadChildren: './employee/employee.module#EmployeeModule'},
   // wild card route
   { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(appRoutes, {preloadingStrategy: PreloadAllModules})
+    RouterModule.forRoot(appRoutes, {preloadingStrategy: CustomPreloadingService})
+    // RouterModule.forRoot(appRoutes, {preloadingStrategy: PreloadAllModules})
     // RouterModule.forRoot(appRoutes, {preloadingStrategy: NoPreloading})
   ],
   exports: [RouterModule]
